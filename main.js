2c5c34f0fbac271d1da5e054cc78df66ac8a9fd5
Suprit <suprit6699@gmail.com>
2024-01-25 18:36:29 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed("#text", {
+        strings: ["Frontend Developer", "Web Developer", "Software Engineer"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDelay: 500,
+        loop: true
+    });
+});
+
